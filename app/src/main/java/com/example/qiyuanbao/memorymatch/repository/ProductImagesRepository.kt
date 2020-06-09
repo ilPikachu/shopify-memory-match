8ee8506a85c6fa7e1e7abb9b7761161c56b7ae70
Bob Bao <baoqiyuan777@gmail.com>
2020-06-09 19:04:42 -0400

    chore: explicitly state IO dispatcher

--- app/src/main/java/com/example/qiyuanbao/memorymatch/repository/ProductImagesRepository.kt
+++ app/src/main/java/com/example/qiyuanbao/memorymatch/repository/ProductImagesRepository.kt
@@ -5,6 +5,8 @@ import com.example.qiyuanbao.memorymatch.extension.toDomainModel
 import com.example.qiyuanbao.memorymatch.extension.toEntityModel
 import com.example.qiyuanbao.memorymatch.model.ProductImage
 import com.example.qiyuanbao.memorymatch.network.ProductApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class ProductImagesRepository(private val database: AppDatabase) {
     /**
@@ -12,12 +14,18 @@ class ProductImagesRepository(private val database: AppDatabase) {
      * getProductImages and getProductImages DAO methods are both marked as suspend,
      * therefore Room will ensure they are main safe.
      *
-     * Note we don't use Dispatcher.IO because Room will use its own dispatcher to run
+     * Note we could discard Dispatcher.IO because Room will use its own dispatcher to run
      * queries in the background thread, using Dispatcher.IO will actually make your queries slower.
+     *
+     * https://medium.com/androiddevelopers/coroutines-on-android-part-iii-real-work-2ba8a2ec2f45
+     *
+     * But I think it's clearer to developers that this is running on IO by withContext(Dispatchers.IO) explicitly
      */
 
     suspend fun getProductImages(): List<ProductImage> {
-        return database.getProductImageDao().getProductImages().toDomainModel()
+        return withContext(Dispatchers.IO) {
+            database.getProductImageDao().getProductImages().toDomainModel()
+        }
     }
 
 
@@ -26,6 +34,8 @@ class ProductImagesRepository(private val database: AppDatabase) {
             .products
             .map { product -> product.image }
 
-        database.getProductImageDao().insetAll(productImages.toEntityModel())
+        withContext(Dispatchers.IO) {
+            database.getProductImageDao().insetAll(productImages.toEntityModel())
+        }
     }
 }
--- app/src/main/java/com/example/qiyuanbao/memorymatch/repository/UserScoreRepository.kt
+++ app/src/main/java/com/example/qiyuanbao/memorymatch/repository/UserScoreRepository.kt
@@ -4,6 +4,8 @@ import com.example.qiyuanbao.memorymatch.database.AppDatabase
 import com.example.qiyuanbao.memorymatch.extension.toDomainModel
 import com.example.qiyuanbao.memorymatch.extension.toEntityModel
 import com.example.qiyuanbao.memorymatch.model.UserScore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class UserScoreRepository(private val database: AppDatabase) {
     /**
@@ -11,20 +13,29 @@ class UserScoreRepository(private val database: AppDatabase) {
      * all DAO methods are both marked as suspend,
      * therefore Room will ensure they are main safe.
      *
-     * Note we don't use Dispatcher.IO because Room will use its own dispatcher to run
+     * Note we could discard Dispatcher.IO because Room will use its own dispatcher to run
      * queries in the background thread, using Dispatcher.IO will actually make your queries slower.
+     * https://medium.com/androiddevelopers/coroutines-on-android-part-iii-real-work-2ba8a2ec2f45
+     *
+     * But I think it's clearer to developers that this is running on IO by withContext(Dispatchers.IO) explicitly
      */
 
     suspend fun getUserScore(uuid: String): UserScore {
-        return database.getUserScoreDao().getUserScore(uuid).toDomainModel()
+        return withContext(Dispatchers.IO) {
+            database.getUserScoreDao().getUserScore(uuid).toDomainModel()
+        }
     }
 
     suspend fun getAllUserScores(): List<UserScore> {
-        return database.getUserScoreDao().getAllUserScores().toDomainModel()
+        return withContext(Dispatchers.IO) {
+            database.getUserScoreDao().getAllUserScores().toDomainModel()
+        }
     }
 
     suspend fun insertUserScore(userScore: UserScore) {
-        database.getUserScoreDao().insert(userScore.toEntityModel())
+        withContext(Dispatchers.IO) {
+            database.getUserScoreDao().insert(userScore.toEntityModel())
+        }
     }
 
 }
